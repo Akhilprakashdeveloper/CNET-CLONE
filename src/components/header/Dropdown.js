52a420b6872252bdc608aed75e040fed5bb8fbf5
Akhilprakashdeveloper <akhilprakash245@gmail.com>
2023-02-15 15:13:05 +0530

    dropdown added on header

---
+++ src/components/header/Dropdown.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import './dropdown.css';
+
+function Dropdown() {
+  return (
+    <div className='mainDiv'>
+       <div className='innerDiv'>
+        <a href='#'><h5>Tech</h5></a> 
+        <a href='#'><p>Home Entertainment</p></a>
+        <a href='#'><p>Mobile</p></a>
+        <a href='#'><p>Computing</p></a>
+        <a href='#'><p>Services & Software</p></a>
+        <a href='#'><p>Gaming</p></a>
+       </div>
+
+       <div className='innerDiv'>
+        <a href='#'><h5>Money</h5></a>
+        <a href='#'><p>Credit Cards</p></a>
+        <a href='#'><p>Mortgages</p></a>
+        <a href='#'><p>Insurance</p></a>
+        <a href='#'><p>Loans</p></a>
+        <a href='#'><p>Cryptocurrency</p></a>
+        <a href='#'><p>Banking</p></a>
+        <a href='#'><p>Investing</p></a>
+        <a href='#'><p>Taxes</p></a>
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>Home</h5></a>
+       <a href='#'><p>Home Internet</p></a>
+        <a href='#'><p>Home Security</p></a>
+        <a href='#'><p>Kitchen & Household</p></a>
+        <a href='#'><p>Smart Home</p></a>
+        <a href='#'><p>Energy & Utilities</p></a>
+        <a href='#'><p>Yard & Outdoors</p></a>
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>Wellness</h5></a>
+       <a href='#'><p>Sleep</p></a>
+        <a href='#'><p>Nutrition</p></a>
+        <a href='#'><p>Fitness</p></a>
+        <a href='#'><p>Personal Care</p></a>
+        <a href='#'><p>Parenting</p></a>
+        <a href='#'><p>Medical</p></a>
+        <a href='#'><p>Mental Health</p></a>
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>News & Culture</h5></a>
+       <a href='#'><p>Politics</p></a>
+        <a href='#'><p>Social Media</p></a>
+        <a href='#'><p>Privacy</p></a>
+        <a href='#'><p>Misinformation</p></a>
+        <a href='#'><p>Culture</p></a>
+        <a href='#'><p>Internet Culture</p></a>
+        <a href='#'><p>Entertainment</p></a>
+        <a href='#'><p>Sports</p></a>
+        <a href='#'><p>Videos</p></a>
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>Science</h5></a>
+       <a href='#'><p>Climate</p></a>
+        <a href='#'><p>Space</p></a>
+        <a href='#'><p>Biology</p></a>
+        
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>Deals & Reviews</h5></a>
+       <a href='#'><p>Reviews</p></a>
+        <a href='#'><p>Best Products</p></a>
+        <a href='#'><p>Versus</p></a>
+        <a href='#'><p>Gift Guide</p></a>
+        <a href='#'><p>Shopping Extension</p></a>
+       </div>
+
+       <div className='innerDiv'>
+       <a href='#'><h5>Cars</h5></a>
+       <a href='#'><p>Best Cars</p></a>
+        <a href='#'><p>Car Accessories</p></a>
+        <a href='#'><p>Car Reviews</p></a>
+        <a href='#'><p>Car News</p></a>
+        <a href='#'><p>Car Prices</p></a>
+       </div>
+  
+    </div>
+  
+  )
+}
+
+export default Dropdown
+
+
+
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,44 +1,78 @@
-import React from 'react'
-import './header.css'
+import React, { useState } from 'react';
+import './header.css';
+import DropdownMenu from './Dropdown';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [linkText, setLinkText] = useState('More');
+  const [icon, setIcon] = useState(<FaAngleDown />);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    setLinkText(isOpen ? 'More' : 'Close');
+    setIcon(isOpen ? <FaAngleDown /> : <FaAngleUp />);
+  };
+
   return (
-    <nav class="navbar navbar-expand navbar-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#" id='brand'>CNET</a>
-        <span class="navbar-text">Your guide to a better future</span>
-        <ul class="navbar-nav ms-auto d-flex flex-wrap">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Tech</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Money</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Wellness</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">News & Culture</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Science</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Deals & Reviews</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Price Finder</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">More</a>
-          </li>
-        </ul>
+    <div>
+      <nav className="navbar navbar-expand navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#" id='brand'>CNET</a>
+          <span className="navbar-text">Your guide to a better future</span>
+          <ul className="navbar-nav ms-auto d-flex flex-wrap">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Tech</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Money</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Wellness</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">News & Culture</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Science</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Deals & Reviews</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Price Finder</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" onClick={toggleDropdown}>
+                {linkText} {icon}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {isOpen && <DropdownMenu />}
+
+  <div class="container-fluid" style={{marginTop:'3%'}}>
+  <div class="row">
+    <div class="col-12 text-start">
+      <h2 style={{fontSize:"50px",fontWeight:'600',fontFamily: 'Sentinel,serif'}}>Get the tech that takes you places</h2>
+      <p style={{fontWeight:'430',fontSize:'20px'}}>Our experts share the newest products and technologies that get you the most out of your world.</p>
+      <div class="d-flex justify-content-start" style={{marginTop:'3%',borderBottom:'1px solid #dadada',paddingBottom:'1%'}}>
+        <a href="#" class="me-3" style={{textDecoration:"underline 2px solid red",textDecorationSkipInk:'auto',textUnderlinePosition:'under'}}>Tech</a>
+        <a href="#" class="me-3">Home Entertainment</a>
+        <a href="#" class="me-3">Mobile</a>
+        <a href="#" class="me-3">Computing</a>
+        <a href="#" class="me-3">Services &amp; Software</a>
+        <a href="#" class="me-3">Gaming</a>
       </div>
-    </nav>
-  )
+    </div>
+  </div>
+</div>
+</div>
+  );
 }
 
-export default Header
+export default Header;
